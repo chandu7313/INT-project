5530fd3c3b3d360192c79629bf829e308fd6ce82
G.CHANDRA MOHAN <152697164+chandu7313@users.noreply.github.com>
2025-04-14 19:35:42 +0530

    backend checking

--- client/src/pages/Loginpage/Login.jsx
+++ client/src/pages/Loginpage/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     const navigate = useNavigate()
 
     const {backendUrl,setIsLoggedin,getUserData} = useContext(AppContext)
-
+    console.log(backendUrl);
     const [state,setState]=useState('Sign Up')
     const [name,setName]=useState('')
     const [email,setEmail]=useState('')
